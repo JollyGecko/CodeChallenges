3ee1b90b1554cb3a0af3f0888f68eec4eb6e2b0f
JollyGecko <eusouocesar@gmail.com>
2021-06-06 17:25:48 +0100

    power of thor to 190

--- easy/power_of_thor.cpp
+++ easy/power_of_thor.cpp
@@ -1,9 +1,10 @@
 /*
 Challenge: Power Of Thor
 Link: https://www.codingame.com/multiplayer/codegolf/power-of-thor
-Achievements: 238 Code Size (current best is 45)
+Achievements: 190 Code Size (current best is 45)
 */
 #include <iostream>
 using namespace std;int a,b,c,d,x;
-void f(){if(x=b-d){cout<<(x>0?(d++,'S'):(d--,'N'));}if(x=a-c){cout<<(x>0?(c++,'E'):(c--,'W'));}cout<<"\n";}
-int main(){cin>>a>>b>>c>>d;while(1){f();}}
+#include <iostream>
+using namespace std;int a,b,c,d,x;
+int main(){cin>>a>>b>>c>>d;while(1){if(x=b-d){cout<<(x>0?(d++,'S'):(d--,'N'));}if(x=a-c){cout<<(x>0?(c++,'E'):(c--,'W'));}cout<<"\n";}}
