679fd389fa7b94ff57ca55e72e9b977287d3a4ce
JollyGecko <55235674+JollyGecko@users.noreply.github.com>
2021-06-06 17:26:53 +0100

    Update power_of_thor.cpp

--- easy/power_of_thor.cpp
+++ easy/power_of_thor.cpp
@@ -5,6 +5,4 @@ Achievements: 190 Code Size (current best is 45)
 */
 #include <iostream>
 using namespace std;int a,b,c,d,x;
-#include <iostream>
-using namespace std;int a,b,c,d,x;
 int main(){cin>>a>>b>>c>>d;while(1){if(x=b-d){cout<<(x>0?(d++,'S'):(d--,'N'));}if(x=a-c){cout<<(x>0?(c++,'E'):(c--,'W'));}cout<<"\n";}}
